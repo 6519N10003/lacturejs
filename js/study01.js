cc7a75379ca78f882c006cf9637ed0c184db4b38
6519N10003 <s6519N10003@sau.ac.th>
2025-03-04 09:55:49 +0700

    version 0.0.11

--- js/study01.js
+++ js/study01.js
@@ -1,19 +1,18 @@
-console.log('ABC ... ')
-console.log("ABC ... ")
-console.log(123456)
+//study01.js
+//data type in javascript
+//mosetelelee305
+//String
+console.log("ABC..."); console.log('ABC')
+//Number
+console.log(123456) ;
 console.log(115.546)
-console.log(true)
-
-//ตัวแปร
-var a = 10
-let b = 20
-const c = 30 //ไม่สามารถเปลี่ยนค่าได้
-
-let data01 = 'Wow .....'
-let data02 = 123456
-
-
-//เวลาเอาข้อมูลหลากหลายแบบมาใช้ร่วมกัน
-console.log('AAA' + 555 + '     ' + data01 + '     ' + data02 )
-
-console.log( `AAA ${555} ${data01} ${data02}`)
+//Boolean
+console.log(true)  //false
+//Array
+console.log([1, 2, 3, 4, 5, 'Hello...', true])
+//Object
+console.log({fullname:'John', age:20})
+//Null
+console.log(null);
+//Undefined
+console.log(undefined)
--- js/tset03-3-script.js
+++ js/tset03-3-script.js
@@ -1,17 +1,2 @@
-//value of variable
-let data1 = 'Hello ... '; //"Hello ... "
-let data2 = 20;
-let data3 = true;
-//index number 0
-let data4 = [10, 20, 30, 40, 50]
-
-let data5 = {
-    fullname: 'John',
-    age: 20,
-    color: ['red', 'green', 'blue']
-}
-
-
-console.log(data1)
-console.log(data2)
-console.log(data3)
+alert("Heyyyyyyyyyy")
+console.log("Heyyyyyyyyyy");
